6294eaf68991a9bf06e17ee5024dfc5baa828265
Matt Mason <masonmj10@gmail.com>
2021-02-16 19:50:56 -0500

    commit tree ddd3b2a78e5f5717d05eafa3fe6d4ca67cc50a50

--- app.rb
+++ app.rb
@@ -9,10 +9,18 @@ class App < Sinatra::Base
 
   # This is a sample dynamic route.
   get "/hello/:name" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
+    @name = params[:name]
+    "Hello #{@name}!"
   end
 
+  get "/goodbye/:name" do
+    @name = params[:name]
+    "Goodbye, #{@name}."
+  end
   # Code your final two routes here:
+  get "/multiply/:num1/:num2" do 
+    @product = params[:num1].to_i * params[:num2].to_i
+  "#{@product}"
+  end
 
 end
